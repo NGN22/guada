2e45c8343572b63bbafa99194b40ea7cbbdac59c
GuillaumeCRESSOT <guillaume.cressot@ensea.fr>
2019-05-07 00:47:44 +0200

    fix codigo

--- declination_stepper.ino
+++ declination_stepper.ino
@@ -4,47 +4,39 @@
 #include <EEPROM.h>
 #include <Stepper.h>
 
+#define DS3231_I2C_ADDRESS 0x68
+
 const int STEPS_PER_REV = 32;
 const int GEAR_REDUCTION = 64;
+
 const int STEPS_PER_OUTER_REV = STEPS_PER_REV * GEAR_REDUCTION;
-long steps;
-long steps2;
-long angle;
-long angle2;
-long sum_steps;
-long sum_steps2;
-long test_steps;
-long test_steps2;
-long prev_steps;
-long prev_steps2;
 int search_speed = 200;
 struct ts t;
-int horas;
-int minutos;
-int segundos;
 
 Stepper stepmotor(STEPS_PER_REV, 8, 10, 9, 11);
 Stepper stepmotor2(STEPS_PER_REV,4,6,5,7);
 
 void setup() {
-  sum_steps=0;
-  prev_steps = 0;
   Serial.begin(9600);
   Wire.begin();
   DS3231_init(DS3231_INTCN);
   Initializacion();
-  ascencion();
+
+ 
 
   stepmotor.setSpeed(search_speed);
   stepmotor2.setSpeed(search_speed);
+  Initmotor1();
+  Initmotor2();
+  apuntar_estrella();
   
 }
 
 void loop() {
-  readTime();
-  stepmotor1();
-  motor2();
-  calculo();
+  //readTime();
+  //stepmotor1();
+ //motor2();
+ //hora_sid_ms();
 
 }
 void readTime()
@@ -67,8 +59,8 @@ void readTime()
   
 }
 void Initializacion(){
-t.hour=14;
-t.min=53;
+t.hour=19;
+t.min=9;
 t.sec=0;
 t.mday=03;
 t.mon=05;
@@ -81,13 +73,13 @@ void stepmotor1()
   Serial.println("number of steps 1: ");
   while(Serial.available()<=0){}
       
-      angle = Serial.readString().toInt();
-      steps = STEPS_PER_OUTER_REV * angle / 360;
+     long angle = Serial.readString().toInt();
+     long steps = STEPS_PER_OUTER_REV * angle / 360;
     
      Serial.print("steps1:");
      Serial.println(steps);
      stepmotor.step(steps);
-     sum_steps=sum_steps+steps;
+     long sum_steps=sum_steps+steps;
      if(sum_steps<0)
      {
       sum_steps=2048+sum_steps;
@@ -96,21 +88,19 @@ void stepmotor1()
     EEPROM.write(1,sum_steps/8);
     Serial.print("memoire:");
     Serial.println(EEPROM.read(1));
-
-   prev_steps = steps;
   
 }
 void motor2(){
  Serial.println("number of steps 2: ");
   while(Serial.available()<=0){}
       
-      angle2 = Serial.readString().toInt();
-      steps2 = STEPS_PER_OUTER_REV * angle2 / 360;
+     long angle2 = Serial.readString().toInt();
+     long steps2 = STEPS_PER_OUTER_REV * angle2 / 360;
     
      Serial.print("steps2:");
      Serial.println(steps2);
      stepmotor2.step(steps2);
-     sum_steps2=sum_steps2+steps2;
+     long sum_steps2=sum_steps2+steps2;
      if(sum_steps2<0)
      {
       sum_steps2=2048+sum_steps2;
@@ -120,11 +110,10 @@ void motor2(){
     Serial.print("memoire:");
     Serial.println(EEPROM.read(2));
 
-   prev_steps2 = steps2;
 }
 void Initmotor1()
 {
-  test_steps=EEPROM.read(1)*8;
+  int test_steps=EEPROM.read(1)*8;
   
   Serial.print("Init_steps: ");
   Serial.println(test_steps);
@@ -135,52 +124,94 @@ void Initmotor1()
   EEPROM.write(1,0);
 }
 void Initmotor2(){
-    test_steps2=EEPROM.read(2)*8;
+    int test_steps2=EEPROM.read(2)*8;
   
   Serial.print("Init_steps2: ");
   Serial.println(test_steps2);
   if(test_steps2!=0)
   {
-    stepmotor.step(-test_steps2);
+    stepmotor2.step(-test_steps2);
   }
   EEPROM.write(2,0);
 }
-void ascencion(){
+void apuntar_estrella(){
   Serial.println("Ascencion");
   Serial.print("Horas:");
   while(Serial.available()<=0){}
-  horas = Serial.readString().toInt();
+  long horas = Serial.readString().toInt();
   Serial.println(horas);
   Serial.print("Minutos:");
   while(Serial.available()<=0){}
-  minutos = Serial.readString().toInt();
+  long minutos = Serial.readString().toInt();
   Serial.println(minutos);
   Serial.print("Segundos:");
   while(Serial.available()<=0){}
-  segundos = Serial.readString().toInt();
+  long segundos = Serial.readString().toInt();
   Serial.println(segundos);
+  unsigned long horas_ms=horas*3600000;
+  unsigned long minutos_ms=minutos*60000;
+  unsigned long segundos_ms=segundos*1000;
+  Serial.print("ascencion_ms");
+  long ascencion_ms =horas_ms+minutos_ms+segundos_ms;
+  Serial.println(ascencion_ms);
+  unsigned long mov_steps = ascencion_ms/42072;
+  Serial.print("mov_steps :");
+  Serial.println(mov_steps);
+  stepmotor2.step(mov_steps);
+  EEPROM.write(2,mov_steps/8);
+
+  Serial.println("Declinacion: ");
+  Serial.print("Grados: ");
+  while(Serial.available()<=0){}
+  int grados = Serial.readString().toInt();
+  Serial.println(grados);
+  Serial.print("Minutos: ");
+  while(Serial.available()<=0){}
+  int min_grados = Serial.readString().toInt();
+  Serial.println(min_grados);
+  Serial.print("Segundos: ");
+  while(Serial.available()<=0){}
+  int sec_grados = Serial.readString().toInt();
+  Serial.println(sec_grados);
+  min_grados = min_grados/60;
+  sec_grados=sec_grados/3600;
+  grados=grados+min_grados+sec_grados;
+  Serial.print("total grados: ");
+  Serial.print(grados);
+  int grados_step= grados/0.17578125;
+  Serial.print("step: ");
+  Serial.print(grados_step);
+  stepmotor.step(grados_step);
+  EEPROM.write(1,grados_step/8);
+  
+  
+  
+  
+  
+
+  
+
+  
+  
   
   
 }
 
 void calculo(){
   
+  
+ 
+  
+  
+}
+
+void hora_sid_ms(){
   unsigned long horas_ms=t.hour*3600000;
   Serial.println(horas_ms);
   unsigned long minutos_ms=t.min*60000;
-  Serial.println(minutos_ms);
   unsigned long segundos_ms=t.sec*1000;
-  Serial.println(segundos_ms);
   unsigned long hora_ms =horas_ms+minutos_ms+segundos_ms;
-  
-  Serial.print("hora_ms :");
-  Serial.println(hora_ms);
-  unsigned long  hora_sid_ms=hora_ms*0.997269583;
-  Serial.print("hora_sid_ms :");
-  Serial.println(hora_sid_ms);
-  unsigned long mov_steps = hora_sid_ms/42072;
-  Serial.print("mov_steps :");
-  Serial.println(mov_steps);
-  stepmotor2.step(mov_steps);
-  
+  Serial.print("hora_sideral_ms");
+  unsigned long hora_sideral_ms=hora_ms*0.9972695833;
+  Serial.println(hora_sideral_ms);
 }
